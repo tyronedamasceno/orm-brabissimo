1a1408c581da9a6f8904e881c12cc5ff203acbfa
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-04-14 15:43:13 -0300

    vai

--- core/models.py
+++ core/models.py
@@ -6,6 +6,15 @@ class Viagem(models.Model):
     origem = models.CharField(max_length=100)
     destino = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+    onibus = models.ForeignKey("core.Onibus", on_delete=models.SET_NULL, null=True)
+
 
     def __str__(self) -> str:
         return f'{self.origem}-{self.destino} em {self.data_partida} / R${self.preco}'
+
+
+class Onibus(models.Model):
+    placa = models.CharField(max_length=7)
+    marca = models.CharField(max_length=100)
+    ano = models.IntegerField()
+    empresa = models.CharField(max_length=100)
